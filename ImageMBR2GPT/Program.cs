cc19713ae71e29219036e1656a586e1ed9746c73
LTRData <ol@ltr-data.se>
2024-04-12 00:37:20 +0200

    New features etc
    * Now also possible to convert back MBR
    * Basic syntax help available with --help

--- ImageMBR2GPT/Program.cs
+++ ImageMBR2GPT/Program.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using DiscUtils.Partitions;
 using DiscUtils.Raw;
 using LTRData.Extensions.Formatting;
+using LTRData.Extensions.CommandLine;
+using DiscUtils.Streams;
+using LTRLib.IO;
 
 namespace ImageMBR2GPT;
 
@@ -44,18 +47,72 @@ public static class Program
         }
     }
 
+    private enum TargetLayout
+    {
+        None,
+        MBR,
+        GPT
+    }
+
     public static void UnsafeMain(params string[] args)
     {
-        if (args is null || args.Length == 0)
+        var targetLayout = TargetLayout.None;
+
+        string[]? images = null;
+
+        var cmds = CommandLineParser.ParseCommandLine(args, StringComparer.OrdinalIgnoreCase);
+
+        foreach (var cmd in cmds)
         {
-            throw new ArgumentException("Missing image file path", nameof(args));
+            if (cmd.Key is "g" or "gpt"
+                && !cmds.ContainsKey("m")
+                && !cmds.ContainsKey("mbr"))
+            {
+                targetLayout = TargetLayout.GPT;
+            }
+            else if (cmd.Key is "m" or "mbr"
+                && !cmds.ContainsKey("g")
+                && !cmds.ContainsKey("gpt"))
+            {
+                targetLayout = TargetLayout.MBR;
+            }
+            else if (cmd.Key == "")
+            {
+                images = cmd.Value;
+            }
+            else
+            { 
+                Console.WriteLine(@"ImageMBR2GPT
+Copyright (c) 2023 - 2024 Olof Lagerkvist, LTR Data, https://ltr-data.se
+
+Tool for in-place conversion between MBR and GPT partition table formats.
+Writes a new partition table with the same partition extents as in existing
+partition table, preserving all data within partitions.
+
+Syntax:
+ImageMBR2GPT [--gpt | --mbr] imagefile1 [imagefile2 ...]
+
+--gpt           Write a new GPT partition table replacing existing partition
+-g              table.
+
+--mbr           Write a new MBR partition table replacing existing partition
+-m              table.
+");
+
+                return;
+            }
         }
 
-        foreach (var arg in args)
+        if (images is null || images.Length == 0)
         {
-            Console.WriteLine($"Opening '{arg}'...");
+            throw new InvalidOperationException("Missing image file path");
+        }
 
-            using var disk = OpenVirtualDisk(arg, FileAccess.ReadWrite);
+        foreach (var image in images)
+        {
+            Console.WriteLine($"Opening '{image}'...");
+
+            using var disk = OpenVirtualDisk(image, FileAccess.ReadWrite);
 
             var partition_table = disk.Partitions
                 ?? throw new NotSupportedException("No partitions detected.");
@@ -70,10 +127,30 @@ public static class Program
 
             foreach (var (i, p) in extents)
             {
-                Console.WriteLine($"Partition {i}, {p.TypeAsString}, offset sector {p.FirstSector}, number of sectors {p.SectorCount} ({SizeFormatting.FormatBytes(p.SectorCount * disk.Geometry.BytesPerSector)})");
+                string? active = null;
+
+                if (p is BiosPartitionInfo bios_part
+                    && bios_part.IsActive)
+                {
+                    active = ", active";
+                }
+
+                Console.WriteLine($"Partition {i}, {p.TypeAsString}, offset sector {p.FirstSector}, number of sectors {p.SectorCount} ({SizeFormatting.FormatBytes(p.SectorCount * disk.Geometry.BytesPerSector)}){active}");
             }
 
-            Console.WriteLine("Do you want to replace the current partition table with a new GPT partition table? (y/N)");
+            if (targetLayout == TargetLayout.None)
+            {
+                return;
+            }
+
+            if (targetLayout == TargetLayout.MBR
+                && extents.Count(extent => extent.p.GuidType != GuidPartitionTypes.MicrosoftReserved) > 4)
+            {
+                throw new NotSupportedException("Cannot convert disks with more partitions than 4 to MBR layout");
+            }
+
+            Console.WriteLine($"Do you want to replace the current partition table with a new {targetLayout} partition table? (y/N)");
+            
             var keychar = Console.ReadKey().KeyChar;
             Console.WriteLine();
             if (keychar != 'y')
@@ -83,23 +160,69 @@ public static class Program
             }
 
             Console.WriteLine("Creating new partition table...");
-            var new_table = GuidPartitionTable.Initialize(disk);
+
+            PartitionTable new_table = targetLayout switch
+            {
+                TargetLayout.GPT => GuidPartitionTable.Initialize(disk),
+                TargetLayout.MBR => BiosPartitionTable.Initialize(disk),
+                _ => throw new InvalidOperationException()
+            };
+
             foreach (var (i, p) in extents)
             {
-                var partitionType = GuidPartitionTypes.WindowsBasicData;
-                if (p is GuidPartitionInfo guidPartition)
-                {
-                    partitionType = guidPartition.GuidType;
-                }
-
                 Console.WriteLine($"Creating partition {i}, offset sector {p.FirstSector}, number of sectors {p.SectorCount}");
-                new_table.Create(p.FirstSector, p.LastSector, partitionType, 0, null);
+
+                if (new_table is GuidPartitionTable guid_table)
+                {
+                    var partitionType = GuidPartitionTypes.WindowsBasicData;
+
+                    if (p is GuidPartitionInfo guidPartition)
+                    {
+                        partitionType = guidPartition.GuidType;
+                    }
+
+                    guid_table.Create(p.FirstSector, p.LastSector, partitionType, 0, null);
+                }
+                else if (new_table is BiosPartitionTable bios_table)
+                {
+                    if (p.GuidType == GuidPartitionTypes.MicrosoftReserved)
+                    {
+                        Console.WriteLine("Skipping MSR partition on MBR.");
+                        continue;
+                    }
+
+                    var partitionType = BiosPartitionTypes.Ntfs;
+                    var markActive = false;
+
+                    if (p is BiosPartitionInfo biosPartition)
+                    {
+                        partitionType = biosPartition.BiosType;
+                    }
+                    else if (p is GuidPartitionInfo guidPartition
+                        && guidPartition.GuidType == GuidPartitionTypes.EfiSystem)
+                    {
+                        partitionType = BiosPartitionTypes.EfiSystem;
+                        markActive = true;
+                    }
+
+                    bios_table.CreatePrimaryBySector(p.FirstSector, p.LastSector, partitionType, markActive);
+                }
             }
 
-            Console.WriteLine("Done. The disk now has a GPT partition table.");
+            Console.WriteLine($"Done. The disk now has a {targetLayout} partition table.");
         }
     }
 
-    public static VirtualDisk OpenVirtualDisk(string path, FileAccess access) =>
-        VirtualDisk.OpenDisk(path, access) ?? new Disk(path, access);
+    public static VirtualDisk OpenVirtualDisk(string path, FileAccess access)
+    {
+        if (path.StartsWith(@"\\?\PhysicalDrive", StringComparison.OrdinalIgnoreCase)
+            || path.StartsWith(@"\\.\PhysicalDrive", StringComparison.OrdinalIgnoreCase)
+            || path.StartsWith("/dev/", StringComparison.Ordinal))
+        {
+            var stream = new DiskStream(path, access);
+            return new Disk(stream, ownsStream: Ownership.Dispose);
+        }
+
+        return VirtualDisk.OpenDisk(path, access) ?? new Disk(path, access);
+    }
 }
